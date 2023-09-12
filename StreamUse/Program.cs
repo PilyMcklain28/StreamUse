18b83f6fdfb7178a5bebf7b2042b37a346ff006f
PilyMcklain28 <paolamcklain@gmail.com>
2023-09-12 00:22:21 -0400

    Agregando otro nuevo Proyecto

---
+++ StreamUse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace program 
+{
+    public class program
+    {
+        public static void Main(string[] args) 
+        {           
+                using (Stream fs = new FileStream("./archivo.txt", FileMode.Append, FileAccess.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine("Hola, es mi primera vez manejando ficheros...");
+
+                    }
+                                        
+                }
+         Console.ReadKey();
+        }
+    }
+}
